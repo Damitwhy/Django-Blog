001c5deb98e25b962d40b37fb5545b02d916713e
Kenneth Cox <damitwhy@gmail.com>
2024-07-19 17:32:44 +0000

    moved inport os to top page ut under path lib

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,10 +80,6 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import os
-import dj_database_url
-if os.path.isfile('env.py'):
-    import env
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
