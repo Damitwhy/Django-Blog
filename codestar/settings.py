9aeed380d2d6ab2067a92bf5b3bf522d4716f13c
Kenneth Cox <damitwhy@gmail.com>
2024-07-22 16:11:48 +0000

    added https://*.ws.codeinstitute-ide.net to settings ALLOWED_Hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
